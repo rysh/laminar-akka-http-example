1d75696d018bae64805c4c24fc5175f88f444be6
ishibashi ryuhei <rysh.cact@gmail.com>
2021-12-16 22:55:15 +0900

    Embed a laminar example code(failed to render)

--- client/src/main/scala/rysh/ScalaJSExample.scala
+++ client/src/main/scala/rysh/ScalaJSExample.scala
@@ -7,6 +7,29 @@ import com.raquo.laminar.api.L._
 object ScalaJSExample {
 
   def main(args: Array[String]): Unit = {
+
+
+    val nameVar = Var(initial = "world")
+
+    val rootElement = div(
+      label("Your name: "),
+      input(
+        onMountFocus,
+        placeholder := "Enter your name here",
+        inContext { thisNode => onInput.map(_ => thisNode.ref.value) --> nameVar }
+      ),
+      span(
+        "Hello, ",
+        child.text <-- nameVar.signal.map(_.toUpperCase)
+      )
+    )
+
+    // In most other examples, containerNode will be set to this behind the scenes
+    val containerNode = dom.document.querySelector("#laminar")
+
+
+    render(containerNode, rootElement)
+    
     dom.document.getElementById("scalajsShoutOut").textContent = SharedMessages.itWorks
   }
 }
